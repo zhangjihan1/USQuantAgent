719e9d8d1e7204f9480e3328690ec593ced9b0d5
Jihang Zhang <zhangjihan1@gmail.com>
2025-08-04 23:31:37 +1000

    u

--- backend/api/stock_history/services.py
+++ backend/api/stock_history/services.py
@@ -1,22 +1,20 @@
 import yfinance as yf
 import pandas_ta as ta
-from flask import jsonify
-
+import numpy as np
 def get_stock_history(ticker, start_date, end_date):
-    try:
-        data = yf.Ticker(ticker).history(start=start_date, end=end_date)
-        if data.empty:
-            return jsonify({"error": "No data found for the given ticker and date range."}), 404
+    data = yf.Ticker(ticker).history(start=start_date, end=end_date)
+    if data.empty:
+        return []
 
-        # Calculate indicators
-        data.ta.rsi(length=6, append=True)
-        data.ta.macd(append=True)
-        for length in [5, 10, 20, 30, 50]:
-            data.ta.sma(length=length, append=True)
+    # Calculate indicators
+    data.ta.rsi(length=6, append=True)
+    data.ta.macd(append=True)
+    for length in [5, 10, 20, 30, 50]:
+        data.ta.sma(length=length, append=True)
 
-        data.reset_index(inplace=True)
-        data['Date'] = data['Date'].dt.strftime('%Y-%m-%d')
+    data.reset_index(inplace=True)
+    data['Date'] = data['Date'].dt.strftime('%Y-%m-%d')
 
-        return jsonify(data.to_dict(orient='records'))
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    # Replace NaN with None for valid JSON
+    data = data.replace({np.nan: None})
+    return data.to_dict(orient='records')
